665b11f821608d52133cbfaf1ccceb2fd9f60e50
Widianaptra <gwidianaputra@gmail.com>
2026-07-11 14:08:20 +0800

    add config

---
+++ db_gym/config/con-db.php
@@ -0,0 +1,22 @@
+<?php
+class Database{
+    protected $conn;
+
+    public function __construct(){
+    $this -> connect();
+}
+    protected function connect(){
+        $host = "localhost";
+        $user = "root";
+        $password = "";
+        $db = "db_gym";
+
+       $this->conn = new mysqli($host, $user, $password, $db);
+
+        if($this->conn->connect_error){
+            die("Koneksi Gagal: " .$this->conn->connect_error);
+        }
+    }
+
+}
+?>
